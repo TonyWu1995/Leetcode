4e4361635daf0a7d006532080ab757e9f6ba746a
Tony <gm10285@gmail.com>
2019-09-08 22:47:10 +0800

    add

---
+++ src/com/company/leetcode/LeetCode557.java
@@ -0,0 +1,22 @@
+package com.company.leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode557 {
+
+    public static void main(String[] args) {
+        LeetCode557 leetCode557 = new LeetCode557();
+        String s = "Let's take LeetCode contest";
+        String result = leetCode557.reverseWords(s);
+        System.out.println(result);
+    }
+
+    public String reverseWords(String s) {
+        String splits[] = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String word : splits) {
+            result.append(new StringBuilder(word).reverse().toString()).append(" ");
+        }
+        return result.toString().trim();
+    }
+}
