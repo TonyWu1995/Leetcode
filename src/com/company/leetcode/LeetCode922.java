b8c25b7da466179b1403201facfa1b0a404f7775
Tony <gm10285@gmail.com>
2019-09-30 20:57:02 +0800

    add

---
+++ src/com/company/leetcode/LeetCode922.java
@@ -0,0 +1,28 @@
+package com.company.leetcode;
+
+public class LeetCode922 {
+
+    public static void main(String[] args) {
+        LeetCode922 leetCode922 = new LeetCode922();
+        leetCode922.sortArrayByParityII(new int[]{4, 2, 5, 7});
+    }
+
+    public int[] sortArrayByParityII(int[] A) {
+        int result[] = new int[A.length];
+        int i = 0;
+        for (int j : A) {
+            if (j % 2 == 0) {
+                result[i] = j;
+                i += 2;
+            }
+        }
+        i = 1;
+        for (int j : A) {
+            if (j % 2 == 1) {
+                result[i] = j;
+                i += 2;
+            }
+        }
+        return result;
+    }
+}
