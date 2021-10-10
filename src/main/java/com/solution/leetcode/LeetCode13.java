506b1a002b804d6b6e4ba45be241a95378462bba
Tony <gm10285@gmail.com>
2021-10-10 21:22:14 +0800

    impl

---
+++ src/main/java/com/solution/leetcode/LeetCode13.java
@@ -0,0 +1,88 @@
+package com.solution.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//
+//Symbol       Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+//For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+//
+//Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+//I can be placed before V (5) and X (10) to make 4 and 9.
+//X can be placed before L (50) and C (100) to make 40 and 90.
+//C can be placed before D (500) and M (1000) to make 400 and 900.
+//Given a roman numeral, convert it to an integer.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "III"
+//Output: 3
+//Example 2:
+//
+//Input: s = "IV"
+//Output: 4
+//Example 3:
+//
+//Input: s = "IX"
+//Output: 9
+//Example 4:
+//
+//Input: s = "LVIII"
+//Output: 58
+//Explanation: L = 50, V= 5, III = 3.
+//Example 5:
+//
+//Input: s = "MCMXCIV"
+//Output: 1994
+//Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+public class LeetCode13 {
+
+    static Map<Character, Integer> romanMap;
+
+    static {
+        romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+    }
+
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return -1;
+        }
+        int len = s.length();
+        int result = romanMap.get(s.charAt(len - 1));
+        for (int i = len - 2; i >= 0; i--) {
+            if (romanMap.get(s.charAt(i)) >= romanMap.get(s.charAt(i + 1)))
+                result += romanMap.get(s.charAt(i));
+            else
+                result -= romanMap.get(s.charAt(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LeetCode13 leetCode13 = new LeetCode13();
+        System.out.println(leetCode13.romanToInt("III"));
+        System.out.println(leetCode13.romanToInt("IV"));
+        System.out.println(leetCode13.romanToInt("IX"));
+        System.out.println(leetCode13.romanToInt("MCMXCIV"));
+    }
+
+
+}
